6c756d37d80b4415a3ce332f942baf2278398476
Marcelo Freitas <glacial@gmail.com>
2024-02-28 04:13:45 +0000

    Update initial ideas

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Card from '@/components/card/Card';
+import CardStore from '@/components/card/CardStore';
 import NavBar from '@/components/navbar/Navbar';
 import SearchBar from '@/components/searchbar/SearchBar';
-import Image from 'next/image';
 
 export default function Home() {
   return (
@@ -11,12 +11,21 @@ export default function Home() {
       </header>
       <main className='flex min-h-screen flex-col gap-8 p-8'>
         <SearchBar />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        <div className='stores w-full overflow-x-auto flex flex-nowrap gap-4 pb-4'>
+          <CardStore />
+          <CardStore store='ml' />
+          <CardStore store='magalu' />
+          <CardStore store='americanas' />
+          <CardStore store='submarino' />
+        </div>
+        <div className='grid grid-cols-1 lg:grid-cols-4 gap-8'>
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
       </main>
     </div>
   );
--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,6 +1,6 @@
 const NavBar = () => {
   return (
-    <nav className='p-4 bg-amber-600 text-white font-bold text-center'>
+    <nav className='p-4 bg-slate-600 text-white font-bold text-center lg:text-left'>
       <h1 className=' text-2xl'>🤑 Desconto Porreta</h1>
     </nav>
   );
