eed6abec1f92557e989b316df47b4bd18db3efc1
Marcelo Freitas <glacial@gmail.com>
2024-03-01 02:37:41 +0000

    fix navbar

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,7 +1,9 @@
 const NavBar = () => {
   return (
     <nav className='p-4 bg-slate-600 text-white font-bold text-center lg:text-left fixed w-full z-50'>
-      <h1 className=' text-2xl'>🤑 Desconto Porreta</h1>
+      <div className=' max-w-screen-xl mx-auto px-4'>
+        <h1 className=' text-2xl'>🤑 Desconto Porreta</h1>
+      </div>
     </nav>
   );
 };
