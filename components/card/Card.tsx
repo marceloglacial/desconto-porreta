b9921cb7ea95ab418c613ad42e353fab5351eac3
marceloglacial <glacial@gmail.com>
2024-03-06 00:04:36 +0000

    fix card

--- components/card/Card.tsx
+++ components/card/Card.tsx
@@ -2,8 +2,10 @@ import { FC } from 'react';
 import Image, { ImageProps } from 'next/image';
 import ui from '@/ui';
 import { getCurrency, getDiscount } from '@/services';
+import Link from 'next/link';
 
 interface CardProps {
+  id: number;
   image: ImageProps;
   title: string;
   store: string;
@@ -15,6 +17,7 @@ interface CardProps {
 }
 
 export const Card: FC<CardProps> = ({
+  id,
   title,
   store,
   price,
@@ -24,43 +27,53 @@ export const Card: FC<CardProps> = ({
   return (
     <div className={ui.card.container}>
       <div className={ui.card.image}>
-        <figure className={ui.card.figure}>
-          <Image
-            className={ui.card.image}
-            src={image.src}
-            fill
-            alt={image?.alt || ''}
-            sizes='(max-width: 300px) 100vw, (max-width: 150px) 50vw, 33vw"'
-            priority
-          />
-        </figure>
+        <Link href={`/produto/${id}`}>
+          <figure className={ui.card.figure}>
+            <Image
+              className={ui.card.image}
+              src={image.src}
+              fill
+              alt={image?.alt || ''}
+              sizes='(max-width: 300px) 100vw, (max-width: 150px) 50vw, 33vw"'
+              priority
+            />
+          </figure>
+        </Link>
       </div>
       <div className={ui.card.body}>
         <div className={ui.card.title}>
-          <h2>{title}</h2>
+          <Link href={`/produto/${id}`}>
+            <h2>{title}</h2>
+          </Link>
         </div>
-        <div className={ui.card.store}>
-          <p>vendido por: {store}</p>
-        </div>
-        <div className={ui.card.price.container}>
-          {price?.discount && (
-            <div>
-              <span className={ui.card.price.regular}>
-                {getCurrency(price.regular)}
-              </span>
-              <span className='text-xs text-white bg-orange-400 p-1 rounded ml-2'>
-                {getDiscount(price.regular, price?.discount || 0)}% off
-              </span>
-            </div>
-          )}
-          <span className={ui.card.price.discount}>
-            {getCurrency(price.discount || price.regular)}
-          </span>
-        </div>
+        <Link href={`/produto/${id}`}>
+          <div className={ui.card.store}>
+            <p>vendido por: {store}</p>
+          </div>
+          <div className={ui.card.price.container}>
+            {price?.discount && (
+              <div>
+                <span className={ui.card.price.regular}>
+                  {getCurrency(price.regular)}
+                </span>
+                <span className='text-xs text-white bg-orange-400 p-1 rounded ml-2'>
+                  {getDiscount(price.regular, price?.discount || 0)}% off
+                </span>
+              </div>
+            )}
+            <span className={ui.card.price.discount}>
+              {getCurrency(price.discount || price.regular)}
+            </span>
+          </div>
+        </Link>
         <div className={ui.card.actions.container}>
-          <a href={link} target='_blank' className={ui.button.primary}>
+          <Link
+            href={`/redirect?url=${link}`}
+            target='_blank'
+            className={ui.button.primary}
+          >
             Ir para a loja!
-          </a>
+          </Link>
         </div>
       </div>
     </div>
