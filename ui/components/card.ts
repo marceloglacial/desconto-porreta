b43c2e81d9e1c9a47d40acdeba5d1dadcf0486f4
marceloglacial <glacial@gmail.com>
2024-05-07 15:10:20 -0400

    fix mobile styles

--- ui/components/card.ts
+++ ui/components/card.ts
@@ -2,16 +2,17 @@ const card = {
   container:
     'card rounded-xl overflow-hidden border border-slate-300 shadow-xl bg-white',
   figure: 'card__figure relative w-full aspect-square overflow-hidden bg-white',
-  image: 'card__image object-contain overflow-hidden p-2',
+  image: 'card__image object-contain overflow-hidden p-4  max-h-24 md:max-h-full',
   body: 'card__body p-4 flex flex-col gap-2',
-  title: 'card__title font-bold line-clamp-2',
+  title: 'card__title font-bold line-clamp-3 md:line-clamp-2',
   description: 'card__description line-clamp-3',
-  store: 'card__store text-xs text-slate-600',
+  store: 'card__store text-xs text-slate-600 hidden md:block',
   price: {
     container: 'card__price flex flex-col pt-2 font-bold',
-    regular: 'card__price--regular text-xs text-red-600  line-through',
+    highlight: 'card_highlight flex flex-wrap gap-2 items-center',
+    regular: 'card__price--regular text-xs text-lime-600 line-through ',
+    percentageOff: 'card__price--off text-[10px] md:text-xs text-white bg-green-600 p-1 rounded',
     discount: 'card__price--discount text-xl',
-    percentageOff: 'card__price--off text-xs text-white bg-green-600 p-1 rounded ml-2'
   },
   actions: {
     container: 'card__actions',
--- ui/components/layout.ts
+++ ui/components/layout.ts
@@ -1,20 +1,20 @@
 const layout = {
   container: `layout__container bg-brand-background min-h-screen`,
-  main: `layout__main flex flex-col gap-4 pt-[150px] max-w-screen-xl mx-auto`,
-  footer: `layout__footer flex flex-col gap-4 max-w-screen-xl mx-auto p-4 pt-16 text-sm`,
+  main: `layout__main flex flex-col gap-4 max-w-screen-xl mx-auto max-[400px]:p-4 p-6 max-[400px]:pt-[120px] pt-[120px] md:pt-[150px] `,
+  footer: `layout__footer flex flex-col gap-4 max-w-screen-xl mx-auto pt-8 text-sm p-6`,
   homePage: {
     container: 'home__container grid gap-4',
     stores: 'home__stores w-full overflow-x-auto flex flex-nowrap gap-4 px-4 pl-4 pb-2',
-    content: 'home__content px-4 flex flex-col gap-4',
-    products: 'home__products grid grid-cols-2 lg:grid-cols-5 gap-4'
+    content: 'home__content flex flex-col gap-4',
+    products: 'home__products grid grid-cols-2 sm:grid-cols-3 xl:grid-cols-5 gap-4'
   },
   productpage: {
-    container: 'product__container px-4 pb-8 flex flex-col gap-4',
+    container: 'product__container flex flex-col gap-4',
     title: 'product__title text-2xl md:text-4xl font-bold',
     info: 'product__info grid md:grid-flow-col md:auto-cols-auto gap-8',
     figure:
-      'product__figure card__figure relative h-96 aspect-square overflow-hidden bg-white border-2 rounded-lg shadow-xl',
-    image: 'product__image object-contain overflow-hidden',
+      'product__figure card__figure relative aspect-square overflow-hidden bg-white border-2 rounded-lg shadow-xl',
+    image: 'product__image object-contain w-full h-full overflow-hidden p-4',
     description:
       'pdoruct__description flex flex-col justify-center gap-4 flex-grow',
     vendor: 'product__vendor text-lg text-slate-600',
