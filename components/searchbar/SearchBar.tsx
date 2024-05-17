569d0094c498170e9267350e28f3036b3209acf4
marceloglacial <glacial@gmail.com>
2024-05-17 16:48:31 -0400

    add search

--- components/searchbar/SearchBar.tsx
+++ components/searchbar/SearchBar.tsx
@@ -1,10 +1,33 @@
+'use client'
 import ui from '@/ui'
-import { FC } from 'react'
+import { ChangeEvent, FC, FormEvent, useState } from 'react'
+import { Icon } from '@/components'
+import { useRouter } from 'next/navigation'
 
 export const SearchBar: FC = (): JSX.Element => {
+    const router = useRouter()
+    const [searchTerm, setSearchTerm] = useState<string>('')
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => setSearchTerm(e.target.value)
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        if (!searchTerm) return alert('Favor digitar algo no campo de busca')
+        router.push(`/busca?search=${searchTerm}`)
+    }
     return (
         <div className={ui.searchbar.container}>
-            <input type='search' className={ui.searchbar.input} placeholder='buscar produto' />
+            <form onSubmit={handleSubmit} className='searchbar__form relative'>
+                <input
+                    type='search'
+                    className={ui.searchbar.input}
+                    placeholder='buscar produto'
+                    onChange={handleChange}
+                />
+                <button className='searchbar__buttuon absolute left-3 h-full'>
+                    <Icon size={5} icon={'search'} />
+                    <span className=' sr-only'>Buscar</span>
+                </button>
+            </form>
         </div>
     )
 }
