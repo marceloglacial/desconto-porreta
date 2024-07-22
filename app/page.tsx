fc730eaac25c3c92a6e0d51a4cf172ad2fa7d5f6
marceloglacial <glacial@gmail.com>
2024-07-22 15:43:53 -0400

    add mobile styles for vendors list

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default async function Home() {
     const products = await getProducts({ page: 1 })
     const allProducts: IProduct[] = products.data
     return (
-        <div className='flex flex-wrap gap-8'>
+        <div className='flex flex-wrap gap-4 md:gap-8'>
             <VendorsList />
             <ProductList products={allProducts} />
             <PaginationBar page={products.page} totalPages={products.totalPages} />
--- components/avatar/Avatar.tsx
+++ components/avatar/Avatar.tsx
@@ -7,8 +7,8 @@ export interface AvatarProps {
 
 export const Avatar: FC<AvatarProps> = ({ image }): JSX.Element => {
     return (
-        <div className='avatar__container w-24 aspect-square rounded-full bg-slate-200 overflow-hidden flex items-center justify-center'>
-            <figure className='avatar__figure relative w-16 h-16'>
+        <div className='avatar__container w-16 md:w-24 p-2 md:p-4 aspect-square rounded-full bg-slate-200 overflow-hidden flex items-center justify-center'>
+            <figure className='avatar__figure relative w-full h-full'>
                 <Image
                     {...image}
                     className='avatar__image object-contain'
